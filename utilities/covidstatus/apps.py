2a28aecea6f6d44ff4a8ecdd6f1d9cf66cfc8335
mathewsmonoo <m.monoo@aluno.ifsp.edu.br>
2020-07-21 21:36:05 -0300

    Website ready for apps implementation.

---
+++ utilities/covidstatus/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class CovidstatusConfig(AppConfig):
+    name = 'utilities.covidstatus'
+    verbose_name = "Covid Status"
+    verbose_name_plural = "Covid Status"
---
+++ utilities/covidstatus/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = "covidstatus"
+urlpatterns = [
+    #path(route='',view=views.get_info,name='list'),
+]
